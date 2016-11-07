95a83882c1c9f0260c6425ee3dd146b684e753ee
chilichilling <klaudiapaprika@gmail.com>
2016-11-07 22:32:55 +0100

    best soulution
    best soulution

--- years/years_module.py
+++ years/years_module.py
@@ -1,13 +1,20 @@
-import datetime
-
+import datetime 
 
 def years(age):
-    return
+    age = age 
+    name = str(input("Enter your name here : "))
+    count = int(input("Enter how many time you want to see the resorts :"))  
+    year = datetime.date.today().year
+    years_left=(100-age) + year
+    for count in range (int(count):
+        print(" Hello {} This year is {} and your age is {} so you have {} till you will be 100 years old".format(name ,year, age, years_left))
 
-
+    return years_left
 def main():
-    return
+
+    return 
 
 
 if __name__ == '__main__':
     main()
+
