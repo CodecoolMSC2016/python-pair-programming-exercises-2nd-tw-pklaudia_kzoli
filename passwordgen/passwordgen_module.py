60793f367682a01e926f2678843a660c2d39ee91
karacsonyiz91 <karacsonyiz91@gmail.com>
2016-11-07 21:47:32 +0100

    m

--- passwordgen/passwordgen_module.py
+++ passwordgen/passwordgen_module.py
@@ -1,10 +1,16 @@
-def passwordgen():
-    return
+import string
+import random
+
+def passwordgen(chars=string.ascii_uppercase + "!" + "@" + "#" + "$" + "%" + "^" + "&" + "*" + "(" + ")" + "?" + string.ascii_lowercase + string.digits):
+    print(''.join(random.choice(chars) for _ in range(random.randint(8,16))))
+    return passwordgen
 
 
 def main():
+    passwordgen()
     return
 
 
 if __name__ == '__main__':
     main()
+
