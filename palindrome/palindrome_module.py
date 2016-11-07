521426e1559b288b97d972fbd6f56fe2f2a977fd
chilichilling <klaudiapaprika@gmail.com>
2016-11-07 18:11:51 +0100

    Update palindrome_module.py
    "by:Klaudia"

--- palindrome/palindrome_module.py
+++ palindrome/palindrome_module.py
@@ -1,10 +1,22 @@
-def palindrome(str):
-    return
+import re 
 
+def palindrome(str):
+    str = str.lower()
+    str = re.sub(' ','',str)
+    letters = list(str)
+    palindrome = True
+
+    for letter in letters:
+        if letter == letters[-1]:
+            letters.pop(-1)
+        else:
+            palindrome = False
+            break
+
+    return palindrome
 
 def main():
-    return
-
+   return palindrome
 
 if __name__ == '__main__':
     main()
